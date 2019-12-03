a07746dc7e53f5134eb56869a3e4f096f00a6c44
EDZ <wangxiaoming@yodoo.net.cn>
2019-12-03 14:11:21 +0800

    add fileIO

--- src/test/java/ReadFileInResource.java
+++ src/test/java/ReadFileInResource.java
@@ -3,10 +3,11 @@ import sun.security.util.Resources;
 
 import java.io.*;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 
 public class ReadFileInResource {
 
-        //获取resource中文件,存放到桌面
+        //获取resource中文件--图片,存放到桌面
         @Test
         public void readFileInResource()throws  IOException{
             //获取resource中文件路径
@@ -20,9 +21,40 @@ public class ReadFileInResource {
             int available = inputStream.available();//这个方法可以在读写操作前先得知数据流里有多少个字节可以读取
             byte[] bytes = new byte[available];
             inputStream.read(bytes, 0, available);//在数组的第一个位置写入availble个字节到数组bytes中
-            outputStream = new FileOutputStream(new File("C:\\Users\\EDZ\\Desktop\\boyyom.jpg"));
+            outputStream = new FileOutputStream(new File("C:\\Users\\EDZ\\Desktop\\hell.docx"));
             outputStream.write(bytes);
             outputStream.flush();
         }
 
-}
+    //获取resource中文件--字符,存放到桌面的某个文件中
+    @Test
+    public void readTextFileInResource() throws IOException {
+        String filePath = this.getClass().getClassLoader().getResource("redStar.txt").getPath();
+        File file=new File(filePath);
+        InputStream inputStream = new FileInputStream(file);
+        int availble = inputStream.available();
+        byte[] bytes = new byte[availble];
+        inputStream.read(bytes);
+        //inputStream.close();
+
+        String str =new String(bytes, StandardCharsets.UTF_8);
+        System.out.println(str);
+        OutputStream outputStream = null;
+        outputStream = new FileOutputStream(new File("C:\\Users\\EDZ\\Desktop\\hell.txt"));
+        outputStream.write(bytes);
+        outputStream.flush();
+
+        char c;
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        do {
+             c = (char) br.read();
+            System.out.println(c);
+        } while ( c != 'q');
+        inputStream.close();
+
+    }
+
+
+    }
+
+
